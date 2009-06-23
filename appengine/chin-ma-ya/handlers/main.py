0ef48150a14d9b45b554045ce0f22e964eb1e7ba
takatoshi <takatoshi.ono@gmail.com>
2009-06-24 00:11:55 +0900

    Add new shops in index page.

--- appengine/chin-ma-ya/handlers/main.py
+++ appengine/chin-ma-ya/handlers/main.py
@@ -1,8 +1,12 @@
 from common import RequestHandler, App
+from models import Shop
 
 class MainPage(RequestHandler):
     def get(self):
-        self.render_response('index.html', {})
+        new_shops = Shop.gql('ORDER BY id DESC').fetch(limit=10)
+        self.render_response('index.html', {
+            'new_shops': new_shops
+            })
 
 if __name__ == "__main__":
     app = App([('/', MainPage),
