ff24fd64a9fa692b8edbca41b9d5cbfc1fa2cb5a
takatoshi <takatoshi.ono@gmail.com>
2009-07-03 00:10:04 +0900

    陳麻家のURL変更に対応. エリアに四国を追加.

--- perl/lib/Chinmaya/Scraper/URI.pm
+++ perl/lib/Chinmaya/Scraper/URI.pm
@@ -7,7 +7,7 @@ use base qw/ Class::Accessor::Fast/;
 use URI;
 
 __PACKAGE__->mk_accessors(
-    qw/ ht k kh tk chu ko /
+    qw/ ht k kh tk chu ko s /
 );
 
 sub new {
@@ -20,12 +20,13 @@ sub new {
 sub init {
     my $self = shift;
 
-    $self->ht( URI->new('http://www.chin-ma-ya.com/tenpo/ht.html') );  # 北海道・東北
-    $self->k( URI->new('http://www.chin-ma-ya.com/tenpo/k.html') );   # 関東
-    $self->kh( URI->new('http://www.chin-ma-ya.com/tenpo/kh.html') );  # 甲信越・北陸
-    $self->tk( URI->new('http://www.chin-ma-ya.com/tenpo/tk.html') );  # 東海・近畿
-    $self->chu( URI->new('http://www.chin-ma-ya.com/tenpo/chu.html') ); # 中国
-    $self->ko( URI->new('http://www.chin-ma-ya.com/tenpo/ko.html') );  # 九州・沖縄
+    $self->ht( URI->new('http://www.chin-ma-ya.net/tenpo/ht.html') );  # 北海道・東北
+    $self->k( URI->new('http://www.chin-ma-ya.net/tenpo/k.html') );   # 関東
+    $self->kh( URI->new('http://www.chin-ma-ya.net/tenpo/kh.html') );  # 甲信越・北陸
+    $self->tk( URI->new('http://www.chin-ma-ya.net/tenpo/tk.html') );  # 東海・近畿
+    $self->chu( URI->new('http://www.chin-ma-ya.net/tenpo/chu.html') ); # 中国
+    $self->ko( URI->new('http://www.chin-ma-ya.net/tenpo/ko.html') );  # 九州・沖縄
+    $self->s( URI->new('http://www.chin-ma-ya.net/tenpo/s.html') ); # 四国
 }
 
 1;
