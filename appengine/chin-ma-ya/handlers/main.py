e98354bca55d76894f814eb6f53f2991c30b91b8
takatoshi <takatoshi.ono@gmail.com>
2009-10-07 21:37:03 +0900

    Redirect to chinma-ya.appspot.com

--- appengine/chin-ma-ya/handlers/main.py
+++ appengine/chin-ma-ya/handlers/main.py
@@ -3,10 +3,11 @@ from models import Shop
 
 class MainPage(RequestHandler):
     def get(self):
-        new_shops = Shop.gql('ORDER BY id DESC').fetch(limit=10)
-        self.render_response('index.html', {
-            'new_shops': new_shops
-            })
+        self.redirect("http://chinma-ya.appspot.com/", True)
+        #new_shops = Shop.gql('ORDER BY id DESC').fetch(limit=10)
+        #self.render_response('index.html', {
+        #    'new_shops': new_shops
+        #    })
 
 if __name__ == "__main__":
     app = App([('/', MainPage),
