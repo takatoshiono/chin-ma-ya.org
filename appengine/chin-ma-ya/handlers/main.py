c9b7a7e76056e435776a7b2f48c12941befc7d83
takatoshi <takatoshi.ono@gmail.com>
2009-10-07 21:48:01 +0900

    Redirect all page.

--- appengine/chin-ma-ya/handlers/main.py
+++ appengine/chin-ma-ya/handlers/main.py
@@ -2,15 +2,15 @@ from common import RequestHandler, App
 from models import Shop
 
 class MainPage(RequestHandler):
-    def get(self):
-        self.redirect("http://chinma-ya.appspot.com/", True)
+    def get(self, path):
+        self.redirect("http://chinma-ya.appspot.com/" + path, True)
         #new_shops = Shop.gql('ORDER BY id DESC').fetch(limit=10)
         #self.render_response('index.html', {
         #    'new_shops': new_shops
         #    })
 
 if __name__ == "__main__":
-    app = App([('/', MainPage),
+    app = App([('/(?P<path>.*)', MainPage),
               ])
     app.run()
 
